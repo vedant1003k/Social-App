50fcaaa5ff48b42ec040a62e13be295707c265e6
Vedant Kashyap <varun.567.kashyap@gmail.com>
2025-01-24 16:18:50 +0530

    Update Messenger.jsx

--- client/src/pages/Messenger/Messenger.jsx
+++ client/src/pages/Messenger/Messenger.jsx
@@ -22,7 +22,7 @@ const Messenger = () => {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io("https://social-app-socket-b8p4.onrender.com");
+    socket.current = io("https://social-app-1-75ux.onrender.com");
     socket.current.on("getMessage", (data) => {
       setarrivalMessages({
         sender: data.senderId,
